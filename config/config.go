6c997aa2291003a0c57354ceec97a203d23020e1
Joe Sirianni <joe.sirianni@observiq.com>
2024-12-23 11:19:01 -0500

    begin working on config

---
+++ config/config.go
@@ -0,0 +1,87 @@
+package config
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	// EnvServerList is the environment variable that
+	// contains the list of DayZ server hostnames or IP
+	// addresses to monitor. The list should be comma
+	// separated, in the form of "host1:port1,host2:port2".
+	// e.g: 50.108.116.1:2324,50.108.116.1:5700
+	EnvServerList = "DAYZ_SERVER_LIST"
+)
+
+// ServerEndpoint is the endpoint of the DayZ server.
+type ServerEndpoint struct {
+	host string
+	port string
+}
+
+// Config is the configuration for the DayZ server monitor.
+type Config struct {
+	// ServerList is the list of DayZ server hostnames or IP
+	ServerList []ServerEndpoint
+}
+
+// ReadEnv reads the configuration from the environment variables
+// and returns the configuration. If there is an error reading
+// the environment or the configuration is invalid, it returns
+// an error with a nil configuration. The configuration will
+// never be nil if there is no error.
+func ReadEnv() (*Config, error) {
+	c := &Config{}
+
+	serverList, err := readServerList()
+	if err != nil {
+		return nil, fmt.Errorf("read server list: %w", err)
+	}
+	c.ServerList = serverList
+
+	return c, nil
+}
+
+func readServerList() ([]ServerEndpoint, error) {
+	v, ok := os.LookupEnv(EnvServerList)
+	if !ok {
+		return nil, fmt.Errorf("%s is a required option", EnvServerList)
+	}
+
+	if v == "" {
+		return nil, fmt.Errorf("%s is empty", EnvServerList)
+	}
+
+	endpoints := strings.Split(v, ",")
+	serverEndPoints := make([]ServerEndpoint, len(endpoints))
+
+	for i, endpoint := range endpoints {
+		host, port, err := net.SplitHostPort(endpoint)
+		if err != nil {
+			return nil, fmt.Errorf("invalid server endpoint %q while reading %s: %v", endpoint, EnvServerList, err)
+		}
+
+		if host == "" {
+			return nil, fmt.Errorf("invalid server endpoint %q while reading %s: host is empty", endpoint, EnvServerList)
+		}
+
+		if port == "" {
+			return nil, fmt.Errorf("invalid server endpoint %q while reading %s: port is empty", endpoint, EnvServerList)
+		}
+
+		if _, err := strconv.Atoi(port); err != nil {
+			return nil, fmt.Errorf("invalid server endpoint %q while reading %s: port is not a number", endpoint, EnvServerList)
+		}
+
+		serverEndPoints[i] = ServerEndpoint{
+			host: host,
+			port: port,
+		}
+	}
+
+	return serverEndPoints, nil
+}
---
+++ config/config_test.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadEnv(t *testing.T) {
+	cases := []struct {
+		name     string
+		env      map[string]string
+		expected *Config
+		err      error
+	}{
+		{
+			"valid configuration",
+			map[string]string{
+				EnvServerList: "10.99.1.10:5000,10.99.1.11:5001",
+			},
+			&Config{
+				ServerList: []ServerEndpoint{
+					{
+						host: "10.99.1.10",
+						port: "5000",
+					},
+					{
+						host: "10.99.1.11",
+						port: "5001",
+					},
+				},
+			},
+			nil,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Set environment variables for this iteration
+			// Cleanup with defer.
+			for k, v := range tc.env {
+				if err := os.Setenv(k, v); err != nil {
+					t.Fatalf("failed to set environment variable %q: %v", k, err)
+				}
+				defer os.Unsetenv(k)
+			}
+
+			c, err := ReadEnv()
+			if tc.err != nil {
+				require.Error(t, err, "an error was expected")
+				require.ErrorAs(t, err, &tc.err)
+				return
+			}
+			require.NoError(t, err, "an error was not expected")
+
+			require.Equal(t, tc.expected, c, "config mismatch")
+		})
+	}
+}
